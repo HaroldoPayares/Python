27fe2089e2ccba73ec04e398e62f276c9267fce3
Haroldo Payares Salgado <haroldo.payares@outlook.com>
2018-04-13 16:43:59 -0500

    Reduced the read file part into one line

--- LPTHW/ex17.py
+++ LPTHW/ex17.py
@@ -6,8 +6,7 @@ script, from_file, to_file = argv
 print (f"Copying from {from_file} to {to_file}")
 
 # we could do these statements on one line,how?
-in_file = open(from_file)
-indata = in_file.read()
+indata = open(from_file).read()
 
 print(f"The input file is {len(indata)} bytes long")
 
